2dd843dacb5ddb604e16e0a85286c13402cc1f7d
Erwin Ruiz <88996407+erwinruiz@users.noreply.github.com>
2021-10-28 21:35:54 -0700

    Make Home page responsive for desktop design (1440px)

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,9 +1,21 @@
 import classes from "./Header.module.css";
+import { useHistory } from "react-router-dom";
 
 function Header() {
+  const history = useHistory();
+
+  const logoHandler = () => {
+    history.push("/");
+  };
+
   return (
     <header className={classes.header}>
-      <img className={classes.logo} src="./assets/shared/logo.svg" alt="logo" />
+      <img
+        className={classes.logo}
+        src="./assets/shared/logo.svg"
+        alt="logo"
+        onClick={logoHandler}
+      />
     </header>
   );
 }
