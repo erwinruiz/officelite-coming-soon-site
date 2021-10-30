fc0e3d56551a260cff425aee31be92ba0f692ea5
Erwin Ruiz <88996407+erwinruiz@users.noreply.github.com>
2021-10-29 23:40:53 -0700

    Fix backgrounds in Mobile, Tablet and Desktop design

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,26 @@
 import Home from "./Pages/Home";
 import SignUp from "./Pages/SignUp";
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch, useLocation } from "react-router-dom";
+import classes from "./App.module.css";
 
 function App() {
+  const location = useLocation();
+
+  const CSS_CLASSES = `${classes.container} ${
+    location.pathname === "/signup" && classes["bg-signup"]
+  }`;
+
   return (
-    <Switch>
-      <Route path="/" exact>
-        <Home />
-      </Route>
-      <Route path="/signup">
-        <SignUp />
-      </Route>
-    </Switch>
+    <div className={CSS_CLASSES}>
+      <Switch>
+        <Route path="/" exact>
+          <Home />
+        </Route>
+        <Route path="/signup">
+          <SignUp />
+        </Route>
+      </Switch>
+    </div>
   );
 }
 
